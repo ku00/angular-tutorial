2d00646291b5672c84a5f6514fa3cc55e9f2bcb6
ku00 <skentaro36@gmail.com>
2017-12-03 23:52:34 +0900

    5-2. Show messages
    5. Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -10,8 +9,8 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
   selectedHero: Hero;
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
